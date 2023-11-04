870be328e20ba524c88f7de3805793209516609c
Yi Sien (Ian) Ku <57273653+iankuys@users.noreply.github.com>
2023-11-04 01:09:34 -0700

    Neetcode 150

--- pointers/3sum.py
+++ pointers/3sum.py
@@ -1,3 +1,6 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
 def threeSum(self, nums: List[int]) -> List[List[int]]:
 
 	res = set()
