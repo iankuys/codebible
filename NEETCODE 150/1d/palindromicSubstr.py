a6f1785d179866f9372ac66e95b943d63271893d
Yi Sien (Ian) Ku <iankuyisien@gmail.com>
2023-11-29 21:20:24 -0800

    1d dp

---
+++ NEETCODE 150/1d/palindromicSubstr.py
@@ -0,0 +1,45 @@
+# Given a string s, return the number of palindromic substrings in it.
+# A string is a palindrome when it reads the same backward as forward.
+# A substring is a contiguous sequence of characters within the string.
+
+# Example 1:
+# Input: s = "abc"
+# Output: 3
+# Explanation: Three palindromic strings: "a", "b", "c".
+
+# Example 2:
+
+# Input: s = "aaa"
+# Output: 6
+# Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+class Solution:    
+    def expandAndCountPallindromes(self, i, j, s):
+        '''Counts the number of pallindrome substrings from a given center i,j        
+        1. when i=j, it's an odd-lengthed pallindrome string. 
+            eg: for string "aba", i=j=1.
+        2. when i+1=j, it's an even-lengthed pallindrome string. 
+            eg: for string "abba", i=1, j=2.
+        
+        Parameters:
+            i,j - centers from which the code will expand to find number of pallindrome substrings.
+            s - the string in which the code needs to find the pallindrome substrings. 
+        
+        Returns:
+            cnt - The number of pallindrome substrings from the given center i,j       
+        '''
+        
+        length=len(s)
+        cnt=0
+        
+        while 0<=i and j<length and s[i]==s[j]:
+            i-=1
+            j+=1
+            cnt+=1
+        
+        return cnt
+        
+    def countSubstrings(self, s: str) -> int:
+        
+        return sum(self.expandAndCountPallindromes(i,i,s) + self.expandAndCountPallindromes(i,i+1,s) for i in range(len(s)))
+        
---
+++ NEETCODE 150/sliding_window/minWindowSubstr.py
@@ -0,0 +1,44 @@
+# Given two strings s and t of lengths m and n respectively, return the minimum window 
+# substring
+#  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+# The testcases will be generated such that the answer is unique.
+
+# Example 1:
+
+# Input: s = "ADOBECODEBANC", t = "ABC"
+# Output: "BANC"
+# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+# Example 2:
+
+# Input: s = "a", t = "a"
+# Output: "a"
+# Explanation: The entire string s is the minimum window.
+# https://leetcode.com/problems/minimum-window-substring/description/
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        needstr = collections.defaultdict(int)
+        for ch in t:
+            needstr[ch] += 1
+        needcnt = len(t)
+        res = (0, float('inf'))
+        start = 0
+        for end, ch in enumerate(s):
+            if needstr[ch] > 0:
+                needcnt -= 1
+            needstr[ch] -= 1
+            if needcnt == 0:
+                while True:
+                    tmp = s[start]
+                    if needstr[tmp] == 0:
+                        break
+                    needstr[tmp] += 1
+                    start += 1
+                if end - start < res[1] - res[0]:
+                    res = (start, end)
+                needstr[s[start]] += 1
+                needcnt += 1
+                start += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
